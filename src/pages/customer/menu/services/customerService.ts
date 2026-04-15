2270325366aad94857911d28b71b1c4aad4b8b12
ChuDinhTao <chudinhtao123@gmail.com>
2026-04-15 13:06:58 +0700

    refactor code

--- src/pages/customer/menu/services/customerService.ts
+++ src/pages/customer/menu/services/customerService.ts
@@ -3,10 +3,6 @@ import { API_ROUTES } from '@/shared/constants/API_ROUTES'
 import { IApiResponse, IPageResponse } from '@/shared/types/IApiResponse'
 import { ICategory, IMenuItem, ICart, ITicketItemRequest } from '../types'
 
-const CUSTOMER_HEADERS = (token: string) => ({
-  headers: { 'X-Session-Token': token }
-})
-
 export const customerService = {
   // Menu
   getCategories: () => 
@@ -22,41 +18,41 @@ export const customerService = {
   openSession: (qrToken: string) =>
     http.post<IApiResponse<{ sessionToken: string }>>('/sessions/open', { qrToken }),
 
-  getSessionOrder: (token: string) =>
-    http.get<IApiResponse<any>>(API_ROUTES.order.sessionOrder, CUSTOMER_HEADERS(token)),
+  getSessionOrder: (token?: string) =>
+    http.get<IApiResponse<any>>(API_ROUTES.order.sessionOrder),
 
   // Cart
-  getCart: (token: string) =>
-    http.get<IApiResponse<ICart>>(API_ROUTES.order.cart, CUSTOMER_HEADERS(token)),
+  getCart: (token?: string) =>
+    http.get<IApiResponse<ICart>>(API_ROUTES.order.cart),
 
   addToCart: (token: string, payload: ITicketItemRequest) =>
-    http.post<IApiResponse<ICart>>(API_ROUTES.order.cartItems, payload, CUSTOMER_HEADERS(token)),
+    http.post<IApiResponse<ICart>>(API_ROUTES.order.cartItems, payload),
 
   updateCartItem: (token: string, cartItemId: string, quantity: number, note: string) =>
-    http.put<IApiResponse<ICart>>(`${API_ROUTES.order.cartItems}/${cartItemId}`, { quantity, note }, CUSTOMER_HEADERS(token)),
+    http.put<IApiResponse<ICart>>(`${API_ROUTES.order.cartItems}/${cartItemId}`, { quantity, note }),
 
   deleteCartItem: (token: string, cartItemId: string) =>
-    http.delete<IApiResponse<ICart>>(`${API_ROUTES.order.cartItems}/${cartItemId}`, CUSTOMER_HEADERS(token)),
+    http.delete<IApiResponse<ICart>>(`${API_ROUTES.order.cartItems}/${cartItemId}`),
 
   // Tickets & Others
-  submitOrder: (token: string, note?: string) =>
-    http.post<IApiResponse<any>>(API_ROUTES.order.tickets, { note }, CUSTOMER_HEADERS(token)),
+  submitOrder: (token?: string, note?: string) =>
+    http.post<IApiResponse<any>>(API_ROUTES.order.tickets, { note }),
 
   requestPayment: (token: string, paymentMethod: string) =>
-    http.post<IApiResponse<any>>('/orders/request-payment', { paymentMethod }, CUSTOMER_HEADERS(token)),
+    http.post<IApiResponse<any>>('/orders/request-payment', { paymentMethod }),
 
   createPayOSLink: (token: string, orderId: string, amount: number) =>
-    http.post<any>(`/payments/payos/create/${orderId}?amount=${amount}`, {}, CUSTOMER_HEADERS(token)),
+    http.post<any>(`/payments/payos/create/${orderId}?amount=${amount}`, {}),
 
   callStaff: (token: string, callType: string, note?: string) =>
-    http.post<IApiResponse<any>>('/staff-calls', { callType, note }, CUSTOMER_HEADERS(token)),
+    http.post<IApiResponse<any>>('/staff-calls', { callType, note }),
 
   cancelTicket: (token: string, ticketId: string) =>
-    http.patch<IApiResponse<any>>(`${API_ROUTES.order.root}/session/tickets/${ticketId}/cancel`, {}, CUSTOMER_HEADERS(token)),
+    http.patch<IApiResponse<any>>(`${API_ROUTES.order.root}/session/tickets/${ticketId}/cancel`, {}),
 
   cancelItem: (token: string, itemId: string) =>
-    http.patch<IApiResponse<any>>(`${API_ROUTES.order.root}/session/items/${itemId}/cancel`, {}, CUSTOMER_HEADERS(token)),
+    http.patch<IApiResponse<any>>(`${API_ROUTES.order.root}/session/items/${itemId}/cancel`, {}),
 
   applyPromotion: (token: string, code: string) =>
-    http.patch<IApiResponse<any>>(`${API_ROUTES.order.root}/session/promotion`, { code }, CUSTOMER_HEADERS(token)),
+    http.patch<IApiResponse<any>>(`${API_ROUTES.order.root}/session/promotion`, { code }),
 }
